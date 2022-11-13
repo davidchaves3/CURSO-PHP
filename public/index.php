b6d9c15a252f5f4383c16ab4768472adf6fe4d85
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-13 17:30:40 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,6 @@
 <?php
 
- $title = 'Home';
+$title = 'Home';
+$name = 'David';
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $title;   ?></title>
-</head>
-<body>
-  
-</body>
-</html>
+echo 'Meu nome é David';
